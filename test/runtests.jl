bc233d642192dce330faad2f529bb14aed6f1ee9
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2026-07-03 17:58:02 +0200

    Implemented two integration tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,15 +22,89 @@ using PythonCall
         @test ver isa String
     end
 
-    # TODO write actual test
-    # @testset "Data Conversion (NumPy/Julia)" begin
-    #     # Testing that a Julia array goes in and a result comes out
-    #     data = Float32[1.0, 2.0, 3.0]
+    @testset "Run a kernel" begin
+        # Defining a simple vector add kernel
+        kernel_code = """
+        using KernelAbstractions
 
-    #     # Test a wrapped function
-    #     # PythonCall will wrap the Julia array as a NumPy-compatible object
-    #     result = KernelTuner.tune_kernel(data)
+        @kernel function vector_add!(C, A, B, n)
+            i = @index(Global)
+            if i <= n
+                @inbounds C[i] = A[i] + B[i]
+            end
+        end
+        """
 
-    #     @test result isa Py  # Or whatever type you expect back
-    # end
+        # Verify the code string was created successfully
+        @test !isempty(kernel_code)
+
+        # Set up the arguments for the kernel
+        size = 10000000
+        rsize = size ÷ 4  # Repeat the base array to reach the desired size
+        a = repeat(Float32[1, 2, 3, 4], outer=rsize)
+        b = repeat(Float32[10, 20, 30, 40], outer=rsize)
+        c = zeros(Float32, size)
+        arguments = [c, a, b, Int32(size)]
+
+        # Run the kernel
+        results = KernelTuner.run_kernel(
+            "vector_add!",
+            kernel_code,
+            (size,),
+            arguments,
+            [],
+            lang="Julia",
+        )
+
+        # Verify the results
+        answer = repeat(Float32[11, 22, 33, 44], outer=rsize)
+        @test length(results) == length(arguments)
+        @test length(results[1]) == length(arguments[1]) == length(answer)
+        @test results[1] == answer
+
+    end
+
+    @testset "Tune a kernel" begin
+        # Defining a simple vector add kernel
+        kernel_code = """
+        using KernelAbstractions
+
+        @kernel function vector_add!(C, A, B, n, block_size_x)
+            i = @index(Global)
+            if i <= n
+                @inbounds C[i] = A[i] + B[i]
+            end
+        end
+        """
+
+        # Set up the arguments and tunable parameters
+        size = 10000000
+        rsize = size ÷ 4  # Repeat the base array to reach the desired size
+        a = repeat(Float32[1, 2, 3, 4], outer=rsize)
+        b = repeat(Float32[10, 20, 30, 40], outer=rsize)
+        c = zeros(Float32, size)
+        tune_params = [
+            ("block_size_x", [32, 64, 128, 256, 512])
+        ]
+
+        # Tune the kernel
+        results = KernelTuner.tune_kernel(
+            "vector_add!",
+            kernel_code,
+            (size,),
+            [c, a, b, Int32(size)],
+            tune_params,
+            grid_div_x=1, grid_div_y=1,
+            block_size_names=["block_size_x"],
+            lang="Julia",
+            answer=[repeat(Float32[11, 22, 33, 44], outer=rsize), nothing, nothing, nothing],
+        )
+
+        # Verify the results
+        @test results[1] isa Tuple
+        @test length(results[1]) == length(tune_params[1][2])  # number of configurations tested
+        @test results[2] isa Dict
+        @test "best_config" in keys(results[2])
+
+    end
 end
